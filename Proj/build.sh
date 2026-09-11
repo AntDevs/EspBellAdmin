bc30ac6fe315e2f6d208a3da6eb5d354ae9108e4
topasta <gurevichDevs@gmail.com>
2026-09-11 23:28:12 +0300

    fix build remove old build folder

--- Proj/build.sh
+++ Proj/build.sh
@@ -2,5 +2,11 @@
 
 cd /media/topasta/Dev/git/EspBellAdmin/Proj/source
 find . -name "*.py" -exec /media/topasta/Dev/ESP32/firmware/cust_gen/esp_micropython/micropython/mpy-cross/build/mpy-cross -O2 {} \;
+rm -rf ../build
 rsync -aR --remove-source-files --include="*/" --include="*.mpy" --exclude="*" ././ ../build/
-rsync -aR --include="*/" --include="*.html" --include="*.css" --include="*.js" --include="*.json" --exclude="*" ././ ../build/
+rsync -aR --exclude="*.py" ././ ../build/
+
+
+# rync -aR --remove-source-files --include="*/" --include="*.mpy" --exclude="*" ././ ../build/
+# rsync -aR --include="*/" --include="*.{html,css,js,json}" --exclude="*" ././ ../build/
+# rsync -aR --exclude="*.py" --include="*/" --include="*"  ././ ../build/
